1d000dc31050819e448445e4b83158f2310b6dd0
Alex Antufiev <12766383+AlexAntufiev@users.noreply.github.com>
2017-12-04 16:34:36 +0300

    Add files via upload

--- src/ru/mail/polis/structures/SimpleInteger.java
+++ src/ru/mail/polis/structures/SimpleInteger.java
@@ -4,19 +4,33 @@ package ru.mail.polis.structures;
  * Created by Nechaev Mikhail
  * Since 12/11/2017.
  */
-public class SimpleInteger implements Numerical<SimpleInteger> {
+public class SimpleInteger implements Numerical, Comparable<ru.mail.polis.structures.SimpleInteger> {
 
     private static final int DIGIT_COUNT = 10;
 
-//    private final todo data;
-//    private final int length;
+    private final int[] data;
+    private final int length;
 
     public SimpleInteger(Integer data) throws IllegalArgumentException {
         if (data == null) {
             throw new IllegalArgumentException("Source must be not null");
         }
-//        this.data = todo
-//        this.length = todo
+        int[] temp = new int[32];
+        int length = 0;
+        for (int i = 0; i < 32; i++) {
+            if (data == 0)
+                break;
+            temp[i] = data % DIGIT_COUNT;
+            data /= DIGIT_COUNT;
+        }
+        this.data = temp;
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] == 0) {
+                length = i;
+                break;
+            }
+        }
+        this.length = length;
     }
 
     @Override
@@ -26,8 +40,7 @@ public class SimpleInteger implements Numerical<SimpleInteger> {
         } else if (index >= getDigitCount()) {
             return 0;
         } else {
-            //todo
-            return 0;
+            return data[index];
         }
     }
 
@@ -38,12 +51,24 @@ public class SimpleInteger implements Numerical<SimpleInteger> {
 
     @Override
     public int getDigitCount() {
-        //todo
-        return 0;
+        return length;
     }
 
     @Override
     public int compareTo(SimpleInteger anotherSimpleInteger) {
+        if (data[31] > anotherSimpleInteger.data[31]) {
+            return -1;
+        }
+        if (data[31] < anotherSimpleInteger.data[31]) {
+            return 1;
+        }
+
+        for (int i = data.length - 1; i >= 0; i--) {
+            if (data[i] < anotherSimpleInteger.data[i])
+                return -1;
+            if (data[i] > anotherSimpleInteger.data[i])
+                return 1;
+        }
         return 0;
     }
 }
